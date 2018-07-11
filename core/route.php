5f297864cf113bb3d1c41090f7b79a5a7c693d78
SenCaoKing <1134349927@qq.com>
2018-07-12 00:42:10 +0800

    支持localhost访问

--- core/route.php
+++ core/route.php
@@ -11,59 +11,45 @@ class route{
         // echo 'route is ready!';
 
         /**
-         *
          * 1、隐藏index.php
          * 2、获取URL中的控制器和方法
          * 3、获取URL中的参数
          */
         if(isset($_SERVER['REQUEST_URI']) && $_SERVER['REQUEST_URI'] != '/'){
-            $path = $_SERVER['REQUEST_URI']; // /index/index
-            $patharr = explode('/', trim($path, '/')); // Array ( [0] => index [1] => index )
-            // p($patharr);
+            $path = $_SERVER['REQUEST_URI'];
+            $patharr = explode('/', trim($path,'/'));
+        }else{
+            $patharr = array();
+        }
 
+        if(isset($_SERVER['HTTP_HOST']) && ($_SERVER['HTTP_HOST'] == 'localhost' || $_SERVER['HTTP_HOST'] == '127.0.0.1')) {
+            // 去掉项目名称
+            $patharr = array_slice($patharr,1,count($patharr)-1);
+        }
+        if(isset($patharr[0])){
+            if($patharr[0] == 'index.php') {
+                // 去掉 index.php
+                $patharr = array_slice($patharr,1,count($patharr) - 1);
+            }
             if(isset($patharr[0])){
-                if($patharr[0] != 'index.php'){
-                    // 省略了 index.php
-                    $this->ctrl = $patharr[0];
-
-                    if(isset($patharr[1])){
-                        $this->action = $patharr[1];
-                    }else{
-                        $this->action = 'index';
-                    }
-                    $count = count($patharr);
-                    $i=2;
-                    while($i < $count){
-                        $this->params[$patharr[$i]] = $patharr[$i+1];
-                        $i = $i + 2;
-                    }
-                }else{
-                    // 没省略 index.php
-                    if(isset($patharr[1])){
-                        $this->ctrl = $patharr[1];
-                    }
-                    if(isset($patharr[2])){
-                        $this->action = $patharr[2];
-                    }else{
-                        $this->action = 'index';
-                    }
-
-                    $count = count($patharr);
-                    $i = 3;
-                    while($i < $count){
-                        $this->params[$patharr[$i]] = $patharr[$i+1];
-                        $i = $i + 2;
-                    }
-                }
-            }else{
-                $this->ctrl = 'index';
-                $this->action = 'index';
+                $this->ctrl = $patharr[0];
+            }
+            if(isset($patharr[1])){
+                $this->action = $patharr[1];
             }
 
-        }else{
-            $this->ctrl = 'index';
-            $this->action = 'index';
+            $count = count($patharr);
+            $i=2;
+            while($i < $count){
+                if(isset($patharr[$i+1])){
+                    $this->params[$patharr[$i]] = $patharr[$i+1];
+                }
+                $i = $i + 2;
+            }
         }
+
+        p($this->ctrl);
+        p($this->action);
         p($this->params);
     }
 }
