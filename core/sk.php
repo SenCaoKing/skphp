d0377dda0aeb5c2acbd42df7f7a2e53281f11b48
SenCaoKing <1134349927@qq.com>
2018-07-11 23:31:16 +0800

    自动加载类库升级

--- core/sk.php
+++ core/sk.php
@@ -2,9 +2,8 @@
 namespace core;
 
 class sk{
-    public static $class_map = array();
     public static function run(){
         p('hello, world!');
-        $route = new \core\route();
+        $route = new route();
     }
 }
