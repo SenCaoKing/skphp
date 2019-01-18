1e8b63a4c8977e954c83e8e9df6d9759dd92125e
SenCaoKing <1134349927@qq.com>
2019-01-18 15:01:50 +0800

    QueryList采集文章页测试

--- app/ctrl/index_ctrl.php
+++ app/ctrl/index_ctrl.php
@@ -6,30 +6,21 @@ use think\mongo\Query;
 
 class index_ctrl extends \core\render{
     public function index(){
-        $html = <<<STR
-<div id="one">
-    <div class="two">
-        <a href="http://querylist.cc">QueryList官网</a>
-        <img src="http://querylist.com/1.jpg" alt="这是图片">
-        <img src="http://querylist.com/2.jpg" alt="这是图片2">
-    </div>
-    <span>其它的<b>一些</b>文本</span>
-</div>      
-STR;
-        $rules = array(
-            //采集id为one这个元素里面的纯文本内容
-            'text' => array('#one','text'),
-            //采集class为two下面的超链接的链接
-            'link' => array('.two>a','href'),
-            //采集class为two下面的第二张图片的链接
-            'img' => array('.two>img:eq(1)','src'),
-            //采集span标签中的HTML内容
-            'other' => array('span','html')
-        );
+        // 采集文章页
+        $url = 'https://www.cnbeta.com/articles/tech/779841.htm';
 
-        $data = QueryList::Query($html, $rules)->data;
+        // 采集规则
+        $rules = [
+            // 文章标题
+            'title' => ['.title>h1', 'text'],
+            // 发布日期
+            'date' => ['.meta>span:eq(0)', 'text'],
+            // 文章内容
+            'content' => ['#artibody', 'html']
+        ];
+
+        $data = QueryList::Query($url, $rules)->data;
         p($data);
-
 
         $this->display('index/index.html');
     }
